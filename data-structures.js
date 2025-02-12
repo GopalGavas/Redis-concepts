54dd414dbc744c53b84c53e42e46a59cc071c4c4
GopalGavas <gopalgavas25@outlook.com>
2025-02-12 12:21:15 +0530

    add remaining data structures in redis

--- data-structures.js
+++ data-structures.js
@@ -62,6 +62,7 @@ const dataStructuresRedis = async () => {
 
     // [SETS]
     // {SADD} -> "Add unique elements to the set"
+    /*
     await client.sAdd("languages", [
       "javascript",
       "typescript",
@@ -85,6 +86,64 @@ const dataStructuresRedis = async () => {
 
     const updatedLanguages = await client.sMembers("languages");
     console.log(updatedLanguages);
+    */
+
+    // [SORTED SETS]
+    /*
+    // {ZADD} -> "Add elements to the sorted set with scores"
+    await client.zAdd("leaderboard", [
+      { score: 100, value: "Alice" },
+      { score: 150, value: "Bob" },
+      { score: 120, value: "Alex" },
+      { score: 90, value: "Rebecca" },
+    ]);
+    // {ZRANGE} -> "Get all elements sorted by score (ascending)"
+    const topPlayersAsc = await client.zRange("leaderboard", 0, -1);
+    console.log("LeaderBoard (Ascending Order): ", topPlayersAsc);
+
+    // "ZRANGE with scores"
+    const topPlayersWithScores = await client.zRangeWithScores(
+      "leaderboard",
+      0,
+      -1
+    );
+    console.log("LeaderBoard (with Scores): ", topPlayersWithScores);
+
+    // {ZRANK} - "Get the rank (0-based index) of a player"
+    const rankAlex = await client.zRank("leaderboard", "Alex");
+    console.log("Alex's rank: ", rankAlex + 1);
+
+    // {ZREM} -> "Remove a player from the leaderboard"
+    await client.zRem("leaderboard", "Alice");
+    console.log("Removed alice from the leaderboard");
+
+    const getUpdatedLeaderBoard = await client.zRange("leaderboard", 0, -1);
+    console.log("Updated leaderBoard: ", getUpdatedLeaderBoard);
+    */
+
+    // [HASHES]
+    // {HSET} -> "Set multiple fields in a hash"
+    await client.hSet("product:101", {
+      name: "Product 1",
+      description: "Product 1 description goes here",
+      rating: 5,
+    });
+
+    // {HGET} -> "Retrieve a single field from the hash"
+    const productName = await client.hGet("product:101", "name");
+    console.log(productName);
+
+    // {HGETALL} -> "Retrieve all fields in the hash"
+    const productDetails = await client.hGetAll("product:101");
+    console.log("product detail: ", productDetails);
+
+    // {HDEL} -> "Delete a specific field from the hash"
+    await client.hDel("product:101", "rating");
+    console.log("Product rating deleted");
+
+    // (UPDATED HASH)
+    const updatedProduct = await client.hGetAll("product:101");
+    console.log(updatedProduct);
   } catch (error) {
     console.log(`Connection failed: ${error}`);
   } finally {
